250deed2db7a1d0e78dff93015c3f61d28e3ba35
Kelvin Shen <kelvin.yushen@gmail.com>
2013-02-03 11:08:40 +1300

    Initial commit

---
+++ MaybeApplication/Maybe.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MaybeApplication
+{
+    public class Maybe<TSource>
+    {
+        public bool HasValue { get; set; }
+        public TSource Value { get; set; }
+
+        public Maybe()
+        {
+            this.HasValue = false;
+        }
+
+        public Maybe(TSource value)
+        {
+            if (value == null)
+                this.HasValue = false;
+
+            this.Value = value;
+            this.HasValue = true;
+        }
+
+        public Maybe<TResult> Select<TResult>(Func<TSource, TResult> selector)
+        {
+            if (!HasValue)
+                return new Maybe<TResult>();
+
+            var result = selector.Invoke(Value);
+
+            if (result != null)
+                return new Maybe<TResult>(result);
+
+            return new Maybe<TResult>();
+        }
+
+        public Maybe<TResult> SelectMany<TIntermedia, TResult>(Func<TSource, TIntermedia> selector,
+            Func<TSource, TIntermedia, TResult> selector2)
+        {
+            if (!HasValue)
+                return new Maybe<TResult>();
+
+            var intermedia = selector.Invoke(Value);
+
+            if (intermedia == null)
+                return new Maybe<TResult>();
+
+            var result = selector2.Invoke(Value, intermedia);
+
+            if (result == null)
+                return new Maybe<TResult>();
+
+            return new Maybe<TResult>(result);
+        }
+    }
+}
---
+++ MaybeApplication/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MaybeApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DemoTheOldWayOfNullChecking());
+            Console.WriteLine(DemoTheNewWayOfNullChecking());
+            // Null exception
+            //Console.WriteLine(contact.PersonalPet.PetName);
+            Console.ReadKey();
+        }
+
+        static string DemoTheOldWayOfNullChecking()
+        {
+            Contact contact = new Contact();
+            contact.Name = "Sean";
+            contact.Phone = "12345678";
+            contact.Age = 24;
+            //contact.PersonalPet = new Pet()
+            //{
+            //    PetName = new PetName()
+            //        {
+            //            Name = "My Pet"
+            //        }
+            //};
+
+            if (contact == null)
+                return null;
+
+            if (contact.PersonalPet == null)
+                return null;
+
+            if (contact.PersonalPet.PetName == null)
+                return null;
+
+            var petName = contact.PersonalPet.PetName.Name;
+            if (petName == null)
+                return null;
+
+            return petName;
+        }
+
+        static string DemoTheNewWayOfNullChecking()
+        {
+            Contact contact = new Contact();
+            contact.Name = "Sean";
+            contact.Phone = "12345678";
+            contact.Age = 24;
+            //contact.PersonalPet = new Pet()
+            //{
+            //    PetName = new PetName()
+            //        {
+            //            Name = "My Pet"
+            //        }
+            //};
+
+            // Maybe class allow you to do something similar to int? for the reference types.
+            Maybe<Contact> maybeContact = new Maybe<Contact>(contact);
+
+            var maybePetName = from c in maybeContact
+                          from cp in c.PersonalPet
+                          from pn in cp.PetName
+                          select pn.Name;
+
+            if (maybePetName.HasValue)
+                return maybePetName.Value;
+
+            return null;
+        }
+    }
+
+    public class Contact
+    {
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public int Age { get; set; }
+        public Pet PersonalPet { get; set; }
+    }
+
+    public class Pet
+    {
+        public PetName PetName { get; set; }
+    }
+
+    public class PetName
+    {
+        public string Name { get; set; }
+    }
+}
